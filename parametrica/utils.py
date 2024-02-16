f5a898d90348559d9f55f61aeb2e526a613a5a3f
Fawkes <mypp32@gmail.com>
2024-02-16 16:06:50 +0700

    [*] modified return type

--- parametrica/utils.py
+++ parametrica/utils.py
@@ -1,21 +1,34 @@
 from typing import Dict, Any, Set
 
-def flatten_metadata_keys(dictionary: Dict[str, Any], exclude_secret: bool = True ,prefix = None) -> Set[str]:
+class MetaKey():
+    def __init__(self, key:str, secret:bool) -> None:
+        self.key = key
+        self.secert = secret
+
+    def __hash__(self) -> int:
+        return hash(self.key)
+    
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, MetaKey):
+            return False
+        return self.key == __value.key
+
+def flatten_metadata_keys(dictionary: Dict[str, Any], prefix = None) -> Set[MetaKey]:
     key_set = set()
     for key, value in dictionary.items():
-        if (value['secret'] == True) and exclude_secret:
-            continue
-        key_set.add(key if prefix is None else f'{prefix}.{key}')
+        key_name = key if prefix is None else f'{prefix}.{key}'
+        key_set.add(MetaKey(key_name, secret=value['secret']))
         if isinstance(value, dict) and isinstance(value.get('type', None), dict):
-            for child_key in flatten_metadata_keys(value['type'], exclude_secret, key):
+            for child_key in flatten_metadata_keys(value['type'], key):
                 key_set.add(child_key)
 
     return key_set
 
-def flatten_dataset_keys(dictionary: Dict[str, Any], prefix = None) -> Set[str]:
+def flatten_dataset_keys(dictionary: Dict[str, Any], prefix = None) -> Set[MetaKey]:
     key_set = set()
     for key, value in dictionary.items():
-        key_set.add(key if prefix is None else f'{prefix}.{key}')
+        key_name = key if prefix is None else f'{prefix}.{key}'
+        key_set.add(MetaKey(key_name, secret=False))
         if isinstance(value, dict):
             for child_key in flatten_dataset_keys(value, key):
                 key_set.add(child_key)
